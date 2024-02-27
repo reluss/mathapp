25a4c145bc0e054da71678e6e781b7b1e68287cf
reluss <bashlor@shack.fritz.box>
2024-02-28 00:06:55 +0100

    updated first code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let zahl1 = Math.floor(Math.random() * 11);
+	let zahl2 = Math.floor(Math.random() * 11);
+	$: ergebnis = zahl1 * zahl2;
+	let ergebnisI = '';
+	let falsch = 0;
+	let richtig = 0;
+	let antwort = '';
+	let color = '';
+
+	function pressed(e) {
+		if (e.key === 'Enter') {
+    handleClick();
+	  }
+	}
+
+	function handleClick() {
+		if (ergebnisI !== '') {
+			ergebnisI == ergebnis ? newCalc() : wrong();	
+		}
+	}
+
+	function newCalc() {
+		zahl1 = Math.floor(Math.random() * 11)
+		zahl2 = Math.floor(Math.random() * 11)
+		ergebnisI = '';
+		richtig += 1;
+		antwort = "Richtig";
+		color = '#21C460';
+		setTimeout(async () => {
+				antwortAni()
+			}, 700)
+	}
+	
+	function wrong() {
+		ergebnisI = '';
+		falsch += 1;
+		antwort = "Falsch";
+		color = '#ff3e00';
+		setTimeout(async () => {
+				antwortAni()
+			}, 700)
+	}
+
+	function antwortAni() {
+		antwort = '';
+	}
+
+	
+</script>
+
+<div class="container">
+
+{zahl1} * {zahl2} = <input type="number" bind:value={ergebnisI} placeholder="Ergebnis eintragen" on:keydown={pressed}>
+	
+<br>
+<br>
+
+
+<button on:click={handleClick}>
+	Nächste Aufgabe
+</button>
+<br>
+<br>
+Richtige Antworten: {richtig}
+<br>
+Falsche Antworten: {falsch}
+<br>
+
+
+<h1 style="--theme-color: {color}">{antwort}</h1>
+
+</div>
+
+<style>
+    :global(body) {
+        background: rgb(109, 151, 177);
+    }
+    .container {
+        text-align: center;
+    }
+	h1 {
+		color: var(--theme-color);
+	}
+	input {
+    border: transparent;
+}
+	input:focus{
+    outline: none;
+}
+</style>
